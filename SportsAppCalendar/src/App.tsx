aeeda032c37c8f401725e4804e2a7ad6aed511c6
Chkoo <jesusperedach@gmail.com>
2026-02-21 14:27:56 -0700

    Setup Admin Panel v0.1 (replaces dummy UI)

--- SportsAppCalendar/src/App.tsx
+++ SportsAppCalendar/src/App.tsx
@@ -2,17 +2,20 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./IUs/Main";
 import Menu from "./components/Menu";
-import Example from "./IUs/Example";
+import Example from "./IUs/Example"; //Example component for testing routes
+import Admin from "./IUs/Admin";
 
 function App() {
   return (
     <>
       <Router>
         <Menu />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/admin" element={<Example />} />
-        </Routes>
+        <main className="pt-24">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </main>
       </Router>
     </>
   );
---
+++ SportsAppCalendar/src/IUs/Admin.tsx
@@ -0,0 +1,10 @@
+function Admin() {
+  return (
+    <div className=" bg-red-500 text-white flex items-center justify-between w-auto mx-8 rounded-lg h-[80vh] gap-2">
+      <div className="bg-blue-400 h-full w-46 inline"></div>
+      <div className="bg-green-800 h-full w-full inline"></div>
+    </div>
+  );
+}
+
+export default Admin;
