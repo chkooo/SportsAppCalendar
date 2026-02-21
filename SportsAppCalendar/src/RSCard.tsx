a89c46389186b8910db05c6b773abb19d35461e8
Chkoo <jesusperedach@gmail.com>
2026-02-21 01:54:55 -0700

    Card Component

---
+++ SportsAppCalendar/src/RSCard.tsx
@@ -0,0 +1,29 @@
+function RSCard() {
+  const imgRoute = "/src/assets/loremCancha.svg";
+  return (
+    <div className="bg-gray-100 border border-t-2 border-l-2 border-gray-500 rounded-lg shadow-md p-4 pt-0 font-sans antialiased w-72">
+      <img
+        src={imgRoute}
+        alt=""
+        className="w-full h-32 object-cover rounded-lg"
+      />
+      <h3 className="text-xl font-bold mb-2 tracking-tight">Cancha 1</h3>
+      <p className="text-gray-600 text-base tracking-normal text-left">
+        Futbol Cancha 1
+      </p>
+      <p className="text-gray-600 text-sm tracking-normal text-left">
+        $250/Hora
+      </p>
+      <div className="flex flex-col items-center">
+        <div className="text-gray-200 font-bold bg-green-400 w-fit rounded-full text-sm text-center p-2 border border-t-2 border-l-2 border-gray-800">
+          Disponible
+        </div>
+      </div>
+      <button className="mt-3 bg-blue-400 text-gray-200 border border-t-2 border-l-2 border-zinc-800 rounded-lg font-bold py-2 px-4 hover:bg-blue-500 tracking-tight">
+        Reservar Ahora
+      </button>
+    </div>
+  );
+}
+
+export default RSCard;
