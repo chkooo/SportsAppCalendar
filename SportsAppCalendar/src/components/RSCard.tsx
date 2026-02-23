f59e6124104f2effefb428136e60e7cb6557fd6a
Chkoo <jesusperedach@gmail.com>
2026-02-22 19:29:16 -0700

    UI Changes

--- SportsAppCalendar/src/components/RSCard.tsx
+++ SportsAppCalendar/src/components/RSCard.tsx
@@ -5,21 +5,23 @@ function RSCard() {
       <img
         src={imgRoute}
         alt=""
-        className="w-full h-32 object-cover rounded-lg"
+        className="w-full h-32 object-cover rounded-t-lg"
       />
-      <h3 className="text-xl font-bold mb-2 tracking-tight">Cancha 1</h3>
+      <h3 className="text-xl text-zinc-300 font-bold mb-2 tracking-tight">
+        Cancha 1 {/*Nombre*/}
+      </h3>
       <p className="text-zinc-300 text-base tracking-normal text-left">
-        Futbol Cancha 1
+        Futbol Cancha 1 {/* Descirpcion*/}
       </p>
-      <p className="text-gray-400 text-sm tracking-normal text-left">
-        $250/Hora
+      <p className="text-gray-400 text-sm tracking-normal text-left font-semibold">
+        $250/Hora {/* Precio*/}
       </p>
-      <div className="flex flex-col items-center animate-pulse animate-infinite animate-duration-[1750ms] animate-delay-[750ms] animate-alternate-reverse">
-        <div className="text-gray-200 font-bold bg-emerald-300 w-fit rounded-full text-sm text-center p-2 border border-t-2 border-l-2 border-gray-800 drop-shadow-md [text-shadow:_2px_2px_2px_rgb(0_0_0_/_40%)]">
+      <div className="mt-4 flex flex-col items-center animate-bounce animate-infinite animate-duration-[2000ms] animate-delay-[250ms] animate-ease-in animate-normal">
+        <div className="text-emerald-950 font-bold bg-emerald-300 w-fit rounded-full text-sm text-center p-2 border border-t-2 border-l-2 border-gray-800 drop-shadow-md [text-shadow:4px_4px_4px_rgb(0_0_0/40%)]">
           Disponible
         </div>
       </div>
-      <button className="mt-3 bg-blue-400 text-gray-200 border border-t-2 border-l-2 border-zinc-800 rounded-lg font-bold py-2 px-4 hover:bg-blue-500 tracking-tight">
+      <button className="mt-3 bg-blue-400 text-gray-200 border border-t-2 border-l-2 border-zinc-700 rounded-lg font-bold py-2 px-4 hover:bg-blue-500 tracking-tight">
         Reservar Ahora
       </button>
     </div>
